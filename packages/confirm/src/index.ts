0f8a6e7274b97dca4fde0d6af016856ab023e1f3
Aditya Bisht <adityabisht64@gmail.com>
2023-02-01 00:14:38 +0530

    Minor changes

--- packages/confirm/src/index.ts
+++ packages/confirm/src/index.ts
@@ -1,50 +1,52 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { ethers } from 'ethers';
+import { ethers, Wallet } from 'ethers';
 import openrpcDocument from './openrpc.json';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createAbi, acctAbi } from './abi';
 
-export const createAccount = async (): Promise<string> => {
-  const provider = new ethers.providers.Web3Provider(snap as any);
-  await provider.send('eth_requestAccounts', []);
-  const owner = provider.getSigner();
-  console.log(owner);
-  const result = await snap.request({
-    method: 'snap_confirm',
-    params: [
-      {
-        prompt: 'Create Account',
-        description: 'Create smart account',
-        textAreaContent: `Your smart account will be deployed with methods store ${owner._address}`,
-      },
-    ],
+const getAccount = async () => {
+  const accounts = await window.ethereum.request({
+    method: 'eth_requestAccounts',
   });
-
-  if (result === false) {
-    return '';
-  }
-
-  const acontract = new ethers.Contract(
-    '0xe1bC2DF8Be88939E17Eb5cCfa89Dd7d7fdDaEeF8',
-    createAbi,
-    owner,
-  );
-
-  // const aa = await acontract.createClone(
-  //   '0xa51D9181aC7a8Cc12060483cD42b89216b2d26D4',
-  // );
-  // const rc = await aa.wait();
-  // const event_ = rc.events.find((x: any) => x.event === 'cloneCreated');
-  // const adr = event_.args.clone;
-
-  // await snap.request({
-  //   method: 'snap_manageState',
-  //   params: { operation: 'update', newState: `${adr.toString()}` },
-  // });
-  const adr = '';
-
-  return adr;
+  return accounts[0];
 };
+
+const getProvider = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+  return provider;
+};
+
+// export const createAccount = async (): Promise<string> => {
+//   const account = await getAccount();
+//   const provider = await getProvider();
+//   // const owner = new Wallet(account, provider);
+//   const result = await
+
+// if (result === false) {
+//   return '';
+// }
+
+// const acontract = new ethers.Contract(
+//   '0xe1bC2DF8Be88939E17Eb5cCfa89Dd7d7fdDaEeF8',
+//   createAbi,
+//   owner,
+// );
+
+// const aa = await acontract.createClone(
+//   '0xa51D9181aC7a8Cc12060483cD42b89216b2d26D4',
+// );
+// const rc = await aa.wait();
+// const event_ = rc.events.find((x: any) => x.event === 'cloneCreated');
+// const adr = event_.args.clone;
+
+// await snap.request({
+//   method: 'snap_manageState',
+//   params: { operation: 'update', newState: `${adr.toString()}` },
+// });
+// const adr = '';
+
+// return adr;
+// };
 
 // export const initiateTx = async (): Promise<boolean> => {
 //   const verified = true;
@@ -74,17 +76,32 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
   switch (request.method) {
     case 'rpc.discover':
       return openrpcDocument;
-    // case 'confirm':
-    //   return new Promise(() => {
-    //     initiateTx().then((result) => {
-    //       console.log(result);
-    //     });
-    //   });
     case 'create':
-      return new Promise(() => {
-        createAccount().then((adr: any) => {
-          console.log(adr);
-        });
+      const provider = await getProvider();
+      const account = await getAccount();
+      const owner = new Wallet(account, provider);
+      // const acontract = new ethers.Contract(
+      //   '0xe1bC2DF8Be88939E17Eb5cCfa89Dd7d7fdDaEeF8',
+      //   createAbi,
+      //   owner,
+      // );
+
+      // const aa = await acontract.createClone(
+      //   '0xa51D9181aC7a8Cc12060483cD42b89216b2d26D4',
+      // );
+      // const rc = await aa.wait();
+      // const event_ = rc.events.find((x: any) => x.event === 'cloneCreated');
+      // const adr = event_.args.clone;
+
+      return snap.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: 'Create Account',
+            description: 'Create smart account',
+            textAreaContent: `Your smart account will be deployed with methods store ${adr}`,
+          },
+        ],
       });
     default:
       throw new Error('Method not found.');
