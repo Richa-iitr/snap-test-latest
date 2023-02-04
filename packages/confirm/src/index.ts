7261262f5e079def0aa4ba72e2d753843d3cbcba
Richa-iitr <richa@cs.iitr.ac.in>
2023-02-04 16:00:17 +0530

    updated unitAmt

--- packages/confirm/src/index.ts
+++ packages/confirm/src/index.ts
@@ -1,8 +1,10 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
 import { ethers, Wallet } from 'ethers';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import BigNumber from 'bignumber.js';
 import openrpcDocument from './openrpc.json';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { factoryAbi, acctAbi, erc20Abi } from './abi';
+import { factoryAbi, acctAbi, erc20Abi, uniswapQuoterAbi } from './abi';
 import { erc20tokens, dexs } from './constants';
 
 const getAccount = async () => {
@@ -17,6 +19,22 @@ const getProvider = async () => {
   return provider;
 };
 
+const calculateUnitAmt = (
+  amountIn: any,
+  decimalsIn: any,
+  amountOut: any,
+  decimalsOut: any,
+  slippage: any,
+) => {
+  const resultantAmountOut = amountOut / 10 ** decimalsOut;
+  const amountIn_ = amountIn / 10 ** decimalsIn;
+  let unitAmt: any = resultantAmountOut / amountIn_;
+
+  unitAmt *= (100 - slippage) / 100;
+  unitAmt *= 10 ** 18;
+  return unitAmt;
+};
+
 export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
   // const params = request.params as any[];
   // params[0] = tokenIn address
@@ -25,9 +43,7 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
   // params[3] = order of dex -> hardcode maybe
   // params[4] = unitAmt
   // params[5] = callData
-  const params = [];
-  params[4] = '0'; // not from input
-  params[5] = '0x00'; // calldata
+  const callData = '0x00'; // not needed for uniswapV2, fetch from API for 1inch,0x, paraswap
   switch (request.method) {
     case 'rpc.discover':
       return openrpcDocument;
@@ -43,19 +59,19 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
       if (state) {
         const sca = state.account[0].toString();
         const smartAccount = new ethers.Contract(sca, acctAbi, owner);
-        let inputParams = await snap.request({
+        const inputParams = await snap.request({
           method: 'snap_dialog',
           params: {
             type: 'Prompt',
             fields: {
               title: 'Swap',
               description:
-                'Enter the tokens to be swapped, the amount separated by commas. Example: UNI,WETH,100',
+                'Enter the tokens to be swapped, the amount and the slippage in percentage separated by commas. Example: UNI,WETH,100,0.5',
             },
           },
         });
 
-        let inputDexOrder = await snap.request({
+        const inputDexOrder = await snap.request({
           method: 'snap_dialog',
           params: {
             type: 'Prompt',
@@ -67,30 +83,67 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
           },
         });
 
-        let input = (inputParams as string).split(',');
-        let tokenIn = erc20tokens[input[0] as keyof typeof erc20tokens];
-        let tokenOut = erc20tokens[input[1] as keyof typeof erc20tokens];
+        const input = (inputParams as string).split(',');
+        const tokenIn =
+          erc20tokens[input[0] as keyof typeof erc20tokens].address;
+        const tokenOut =
+          erc20tokens[input[1] as keyof typeof erc20tokens].address;
+        const decimalsIn =
+          erc20tokens[input[0] as keyof typeof erc20tokens].decimals;
+        const decimalsOut =
+          erc20tokens[input[1] as keyof typeof erc20tokens].decimals;
 
-        let inputDexOrder_ = (inputDexOrder as string).split(',');
+        const inputDexOrder_ = (inputDexOrder as string).split(',');
         let dex = [];
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
         for (let i = 0; i < inputDexOrder_.length; i++) {
           dex.push(dexs[inputDexOrder_[i] as keyof typeof dexs]);
         }
 
         // Find decimals of tokenIn
         const tokenInContract = new ethers.Contract(tokenIn, erc20Abi, owner);
-        const decimalsIn = await tokenInContract.decimals();
-        let amtIn = ethers.utils.parseUnits(input[2], decimalsIn);
+        const uniswapQuoter = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+        const uniswapQuoterInstance = new ethers.Contract(
+          uniswapQuoter,
+          uniswapQuoterAbi,
+          owner,
+        );
+        const amtIn: any = ethers.utils.parseUnits(input[2], decimalsIn);
+        const slippage = new BigNumber(input[3]).toNumber();
         // Check allowance
-        let allowance = await tokenInContract.allowance(account, sca);
+        const allowance = await tokenInContract.allowance(account, sca);
         if (allowance.lt(amtIn)) {
-          let tx = await tokenInContract.connect(owner).approve(sca, amtIn);
+          const tx = await tokenInContract.connect(owner).approve(sca, amtIn);
           await tx.wait();
         }
 
-        let txn = await smartAccount
+        const expectedAmountOut = await uniswapQuoterInstance.getAmountOut(
+          amtIn,
+          tokenIn,
+          tokenOut,
+        );
+
+        const unitAmt = calculateUnitAmt(
+          amtIn,
+          decimalsIn,
+          expectedAmountOut,
+          decimalsOut,
+          slippage,
+        );
+        await snap.request({
+          method: 'snap_confirm',
+          params: [
+            {
+              prompt: 'Account created',
+              description: 'You smart contract account address',
+              textAreaContent: `${unitAmt}, ${slippage}, ${expectedAmountOut}, ${amtIn}`,
+            },
+          ],
+        });
+
+        const txn = await smartAccount
           .connect(owner)
-          .swap(dex, tokenIn, tokenOut, amtIn, params[4], params[5]);
+          .swap(dex, tokenIn, tokenOut, amtIn, unitAmt.toFixed(0), callData);
         await txn.wait();
         return await snap.request({
           method: 'snap_notify',
@@ -113,7 +166,7 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
         owner,
       );
 
-      const swap_ = '0x517F886127DF76E61640c7d6C9AFd515bf757220';
+      const swap_ = '0x89012390386aD3337dDd6735B9EAe5e2FAACb21f';
       const batch_ = '0xaca091817aa8fd7863833fea1bf9f8f500eaf795';
 
       const swapSigs = [
@@ -163,9 +216,9 @@ export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
         method: 'snap_confirm',
         params: [
           {
-            prompt: 'Create Account',
-            description: 'Create smart account',
-            textAreaContent: `Your smart account will be deployed with methods store ${acct_}`,
+            prompt: 'Account created',
+            description: 'You smart contract account address',
+            textAreaContent: `Your smart account is deployed to ${acct_}`,
           },
         ],
       });
