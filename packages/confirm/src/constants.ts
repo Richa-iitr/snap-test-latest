56f6ade47cce48164eb25ebbc4cbb597e2801f3d
Richa-iitr <richa@cs.iitr.ac.in>
2023-02-04 16:00:17 +0530

    updated unitAmt

--- packages/confirm/src/constants.ts
+++ packages/confirm/src/constants.ts
@@ -1,7 +1,13 @@
 // ERC-20 token address
 export const erc20tokens = {
-  UNI: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
-  WETH: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+  UNI: {
+    address: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
+    decimals: 18,
+  },
+  WETH: {
+    address: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+    decimals: 18,
+  },
 };
 
 // DEXs
