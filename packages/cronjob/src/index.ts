99faeb4b9784aed7ed7cd64905867b3c6ed06928
Anshul Singh <anshulspidy@gmail.com>
2023-02-05 02:11:15 +0530

    multisig cronjob

--- packages/cronjob/src/index.ts
+++ packages/cronjob/src/index.ts
@@ -1,6 +1,5 @@
 import { OnCronjobHandler } from '@metamask/snaps-types';
 import { panel, text, heading } from '@metamask/snaps-ui';
-import axios from 'axios';
 
 export const onCronjob: OnCronjobHandler = async ({ request }) => {
   switch (request.method) {
@@ -8,11 +7,25 @@ export const onCronjob: OnCronjobHandler = async ({ request }) => {
 
       console.log('Cronjob fired');
 
+      const url : string = 'http://localhost:3000/api/fetch';
+
+      const headers = {
+        'content-type': 'application/json',
+        'X-Access-Key': '',
+      };
+
+      const response = await fetch(url, {
+        method: 'GET',
+        headers,
+      });
+      
+      console.log(response.json());
+
       return snap.request({
-        method: 'snap_dialog',
+        method: 'snap_notify',
         params: {
-          type: 'Alert',
-          content: panel([heading('Cronjob'), text('fired')]),
+          type: 'inApp',
+          message: 'wow',
         },
       });
     default:
