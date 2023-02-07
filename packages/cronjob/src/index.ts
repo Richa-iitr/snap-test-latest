31fe2fa74556b20f97edb8d39da53eb2f11e5cad
Richa-iitr <richa@cs.iitr.ac.in>
2023-02-07 07:50:24 +0530

    Fixes

--- packages/cronjob/src/index.ts
+++ packages/cronjob/src/index.ts
@@ -19,10 +19,10 @@ const processSign = (signer_: string, data_: string, dynamic_: boolean) => {
   let safeSign = {
     signer: signer_,
     data: data_,
-    dynamic: dynamic_
-  }
+    dynamic: dynamic_,
+  };
   return safeSign;
-}
+};
 
 const buildSignatureBytes = (signatures: any) => {
   const SIGNATURE_LENGTH_BYTES = 65;
@@ -31,8 +31,8 @@ const buildSignatureBytes = (signatures: any) => {
     left.signer.toLowerCase().localeCompare(right.signer.toLowerCase());
   });
 
-  let signatureBytes = "0x";
-  let dynamicBytes = "";
+  let signatureBytes = '0x';
+  let dynamicBytes = '';
   for (const sig of signatures) {
     if (sig.dynamic) {
       const dynamicPartPosition = (
@@ -40,13 +40,13 @@ const buildSignatureBytes = (signatures: any) => {
         dynamicBytes.length / 2
       )
         .toString(16)
-        .padStart(64, "0");
+        .padStart(64, '0');
       const dynamicPartLength = (sig.data.slice(2).length / 2)
         .toString(16)
-        .padStart(64, "0");
+        .padStart(64, '0');
       const staticSignature = `${sig.signer
         .slice(2)
-        .padStart(64, "0")}${dynamicPartPosition}00`;
+        .padStart(64, '0')}${dynamicPartPosition}00`;
       const dynamicPartWithLength = `${dynamicPartLength}${sig.data.slice(2)}`;
 
       signatureBytes += staticSignature;
@@ -59,7 +59,12 @@ const buildSignatureBytes = (signatures: any) => {
   return signatureBytes + dynamicBytes;
 };
 
-const executeTx = async (safeAddr: any, owner: any, processed_sign_array: any, txData: any) => {
+const executeTx = async (
+  safeAddr: any,
+  owner: any,
+  processed_sign_array: any,
+  txData: any,
+) => {
   const safeInstance = new ethers.Contract(safeAddr, safeAbi, owner);
   const final_signs = buildSignatureBytes(processed_sign_array);
   // const nonce = await safeInstance.connect(owner).nonce();
@@ -67,26 +72,28 @@ const executeTx = async (safeAddr: any, owner: any, processed_sign_array: any, t
   // Convert txData.value to BigNumber
   txData.value = ethers.BigNumber.from(txData.value);
 
-  console.log('txData.to : ', txData.to)
-  console.log('txData.value : ', typeof txData.value)
-  console.log('final_signs : ', final_signs)
+  console.log('txData.to : ', txData.to);
+  console.log('txData.value : ', typeof txData.value);
+  console.log('final_signs : ', final_signs);
 
-  const tx = await safeInstance.connect(owner).execTransaction(
-    txData.to.toString(),
-    txData.value,
-    "0x",
-    0,
-    0,
-    0,
-    0,
-    "0x0000000000000000000000000000000000000000",
-    "0x0000000000000000000000000000000000000000",
-    final_signs,
-    {}
-  )
+  const tx = await safeInstance
+    .connect(owner)
+    .execTransaction(
+      txData.to.toString(),
+      txData.value,
+      '0x',
+      0,
+      0,
+      0,
+      0,
+      '0x0000000000000000000000000000000000000000',
+      '0x0000000000000000000000000000000000000000',
+      final_signs,
+      {},
+    );
   const receipt = await tx.wait();
   return receipt;
-}
+};
 
 export const onCronjob: OnCronjobHandler = async ({ request }) => {
   switch (request.method) {
@@ -96,8 +103,8 @@ export const onCronjob: OnCronjobHandler = async ({ request }) => {
       const signer = provider.getSigner(account);
 
       const getSignBody = {
-        address : account,
-      }
+        address: account,
+      };
 
       // get sign from backend
       const signResponse = await fetch(
@@ -112,83 +119,96 @@ export const onCronjob: OnCronjobHandler = async ({ request }) => {
       );
 
       // Get JSON response
-      const signResponseJson = await signResponse.json();
+      try {
+        const signResponseJson = await signResponse.json();
 
-      const signature = await signer._signTypedData(signResponseJson.domainData, signResponseJson.type, signResponseJson.params);
-
-      if (signature) {
-        const this_sign = processSign(account, signature, false);
-
-        signResponseJson.signers.push(account);
-        signResponseJson.signatures.push(this_sign);
-        signResponseJson.currentThreshold += 1;
-
-        const updateSignBody = {
-          address: account,
-          signData: signResponseJson,
-          signedBy: signResponseJson.signers,
-          currentThreshold: signResponseJson.currentThreshold,
-        }
-
-        // update sign in backend
-        const updateResponse = await fetch(
-          'https://metamask-snaps.sdslabs.co/api/updateSign',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(updateSignBody),
-          },
+        const signature = await signer._signTypedData(
+          signResponseJson.domainData,
+          signResponseJson.type,
+          signResponseJson.params,
         );
-
-        const getTransactionBody = {
-          address: account,
-        }
-
-        // get transaction from backend
-        const transactionResponse = await fetch(
-          'https://metamask-snaps.sdslabs.co/api/getTransaction',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(getTransactionBody),
-          },
-        );
-
-        try {
-          const transactionResponseJson = await transactionResponse.json();
-
-          const signArray = signResponseJson.signatures;
-
-          const txnData = transactionResponseJson;
-
-          const receipt = await executeTx(transactionResponseJson.safeAddress, signer, signArray, txnData);
-
-          if (receipt.status === 1) {
-            const updateTransactionBody = {
-              address: account,
-              status: 'success',
-            }
-
-            // update transaction in backend
-            const updateTransactionResponse = await fetch(
-              'https://metamask-snaps.sdslabs.co/api/updateTransaction',
-              {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(updateTransactionBody),
+  
+        if (signature) {
+          const this_sign = processSign(account, signature, false);
+  
+          signResponseJson.signers.push(account);
+          signResponseJson.signatures.push(this_sign);
+          signResponseJson.currentThreshold += 1;
+  
+          const updateSignBody = {
+            address: account,
+            signData: signResponseJson,
+            signedBy: signResponseJson.signers,
+            currentThreshold: signResponseJson.currentThreshold,
+          };
+  
+          // update sign in backend
+          const updateResponse = await fetch(
+            'https://metamask-snaps.sdslabs.co/api/updateSign',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
               },
+              body: JSON.stringify(updateSignBody),
+            },
+          );
+  
+          const getTransactionBody = {
+            address: account,
+          };
+  
+          // get transaction from backend
+          const transactionResponse = await fetch(
+            'https://metamask-snaps.sdslabs.co/api/getTransaction',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(getTransactionBody),
+            },
+          );
+  
+          try {
+            const transactionResponseJson = await transactionResponse.json();
+  
+            const signArray = signResponseJson.signatures;
+  
+            const txnData = transactionResponseJson;
+  
+            const receipt = await executeTx(
+              transactionResponseJson.safeAddress,
+              signer,
+              signArray,
+              txnData,
             );
+  
+            if (receipt.status === 1) {
+              const updateTransactionBody = {
+                address: account,
+                status: 'success',
+              };
+  
+              // update transaction in backend
+              const updateTransactionResponse = await fetch(
+                'https://metamask-snaps.sdslabs.co/api/updateTransaction',
+                {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(updateTransactionBody),
+                },
+              );
+            }
+          } catch (e) {
+            console.log('Error : ', e);
+            return;
           }
-        } catch (e) {
-          console.log('Error : ', e);
-          return;
         }
+      } catch (e) {
+        return;
       }
       break;
     default:
